a89e00ddb3f7aaf44759ed4df536925baa05875c
Matthew Moss <matthew@premonition.io>
2015-12-08 08:18:12 +1100

    Progress

--- phone.py
+++ phone.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+import pyglet
+from subprocess import Popen
+import glob
+import random
 
 class keypad():
     # CONSTANTS
@@ -72,18 +76,104 @@ class keypad():
         for j in range(len(self.COLUMN)):
                 GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
+button_press_timeout = 1500
+
 if __name__ == '__main__':
+    pyglet.options['audio'] = ('openal')
     kp = keypad()
+    
+    # GPIO for hangup switch
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    # Global state
+    hangup = True
+    playing = False
+    number = ''
+    lastpress = None
+
+    dialtone_pid = None
+    ring_pid = None
+    music_pid = None
+
+    ring_start_time = None
+    ring_duration = None
+
+    track = None
+
+    # Keypress history
+    history = []
 
     while True:
         # Loop while waiting for a keypress
-        digit = None
-        while digit == None:
-            digit = kp.getKey()
+        hangup = GPIO.input(20)
+        if hangup:
+            # The phone is on the hook
+            history = []
+            print 'Phone on hook'
+            # Safety
+            Popen(['killall', 'mpg123'])
+            dialtone_pid = None
+            ring_pid = None
+            music_pid = None
+            number = ''
+            lastpress = None
+            track = None
+            continue
+
+        # Nothing playing? Play the dialtone
+        if not dialtone_pid and not ring_pid and not music_pid:
+            dialtone_pid = Popen(['mpg123', '--smooth', '--quiet', 'dialtone.mp3'])
+
+        # The dialtone has stopped. Start it again
+        if dialtone_pid and dialtone_pid.poll() != None:
+            dialtone_pid = Popen(['mpg123', '--smooth', '--quiet', 'dialtone.mp3'])
+
+        digit = kp.getKey()
+        if digit != None:
+            if isinstance(digit, int): 
+                if dialtone_pid:
+                    # Restart the dialtone to prevent the annoying cutoff
+                    dialtone_pid.kill()
+                    dialtone_pid = Popen(['mpg123', '--smooth', '--quiet', 'dialtone.mp3'])
+                Popen(['mpg123', '--quiet', str(digit) + 'press.mp3'])
+
+            while (kp.getKey() == digit):
+                time.sleep(0.01)
+
+        if digit not in [None, '#', '*', 'Up', 'Down']:
+            lastpress = int(round(time.time() * 1000))
+            history += [digit]
+
+        if (lastpress and lastpress + button_press_timeout < int(round(time.time() * 1000))):
+            number = ''.join([str (h) for h in history])
+            history = []
+            tracks = glob.glob('tracks/' + number + '_*')
+            if tracks:
+                if dialtone_pid:
+                    dialtone_pid.kill()
+                    dialtone_pid = None
+                if music_pid:
+                    music_pid.kill()
+                    music_pid = None
+                track = tracks[0]
+                ring_pid = Popen(['mpg123', '--smooth', '--quiet', 'ring.mp3'])
+                ring_start_time = int(round(time.time() * 1000))
+                ring_duration = random.randint(500, 2000)
+
+        if ring_pid and ring_start_time + ring_duration < int(round(time.time() * 1000)):
+            ring_pid.kill()
+            ring_pid = None
+            music_pid = Popen(['mpg123', '--smooth', '--quiet', track])
 
         # Print the result
-        print digit
+        print digit, history, number
         sys.stdout.flush()
-        # Debounce
-        time.sleep(0.5)
 
+        # Loop
+        time.sleep(0.01)
+
+#if __name__ == '__main__':
+    #pygame.init()
+    #beautifulSunday = pygame.mixer.Sound('Beautiful-Sunday.ogg')
